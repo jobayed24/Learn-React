988aee78d794abcaa0016a9e21b25ad907a5a45d
jobayed24 <jkhouse117@gmail.com>
2024-05-21 19:16:49 +0600

    SingleField Error: custom error remove when submit button hit

--- src/components/SingleFieldError.jsx
+++ src/components/SingleFieldError.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
 export default function SingleFieldError() {
@@ -13,51 +13,30 @@ export default function SingleFieldError() {
     const onSubmit=(formData)=>{
         console.log(formData)
     }
-    // useEffect(()=>{
-      
-    //         const response=async()=>{
-    //             const data=await fetch('https://dummyjson.com/products');
-    //             return data;
-    //            }
-    //            response();
-    //   if(response.statusCode>200){
-    //     setError('root.serverError',{
-    //         type: response.statusCode
-    //     })
-    //   }
-       
-    //         // if(response.statusCode >200){
-    //         //     setError('root.serverError',{
-    //         //         type: response.statusCode
-    //         //     })
-    //         // }
-       
-    // },[])
+
+    useEffect(()=>{
+        setError('lastName',{
+            types:{
+                required: 'this is requied',
+                minLength: 4
+            }
+        })
+    },[setError])
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Last name:</label>
+        <label>LastName:</label>
         <input 
             {...register('lastName')}
         />
-        <button onClick={async()=>{
-            const response = await fetch('https://dummyjson.com/produc')
-            
-            if(response.status >200){
-                setError('root.serverError',{
-                    type: response.status
-                })
-               
-            }
-        }}>Send Request to server</button>
-       
-            <p>{errors?.root?.serverError?.type==404 && 'Sever error'}</p>
-     
-        {errors.lastName && errors.lastName.types && (
-            <p>{errors.lastName.types.minLength}</p>
+        {errors.lastName && errors.lastName.types.required && (
+            <p>{errors.lastName.types.required}</p>
+        )}
+        {errors.lastName && errors.lastName.types.minLength && (
+            <p>Minium lenght should: {errors.lastName.types.minLength}</p>
         )}
         <input type='submit' />
       </form>
-    </>
+     </>
   )
 }
