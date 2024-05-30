95808b39e8f86ecc570096d4aeda1a95adabf544
jobayed24 <jkhouse117@gmail.com>
2024-05-30 18:52:29 +0600

    added global error and field based error

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -6,16 +6,32 @@ export default function () {
     const {
       register,
       handleSubmit,
+      formState:{errors},
+      setError
     }=useForm()
 
     function onHandleSubmit(formData){
-      console.log(formData)
+      
+      const localUserName={
+        email: "jk@gmail.com",
+        password: "1234"
+      }
+      const found=formData.email===localUserName.email && formData.password===localUserName.password;
+      if(found){
+        console.log(formData)
+      }
+      if(!found){
+        setError('root.random',{
+          type: 'random',
+          message: `the user with email ${formData.email} not found in database`
+        })
+      }
     }
   return (
     <div>
     <form onSubmit={handleSubmit(onHandleSubmit)}>
     <FieldSet label='Enter a login details'>
-        <Field  label='Email' >
+        <Field  label='Email' error={errors.email}>
             <input
                     {...register('email',{required:'Email is required !'})}
                     className='p-2 border box-border w-[300px] rounded-md '
@@ -27,9 +43,12 @@ export default function () {
                 />
 
         </Field>
-        <Field label='Password'>
+        <Field label='Password' error={errors.password}>
             <input
-            {...register('password',{required: 'password is required !'})}
+            {...register('password',{required: 'password is required !',maxLength:{
+              value: '4',
+              message: 'max length 4'
+            }})}
             className='p-2 border box-border w-[300px] rounded-md '
             type='password'
             id='password'
@@ -38,6 +57,7 @@ export default function () {
             
             />
         </Field>
+        {errors.root && <p className='text-red-500'>{errors?.root?.random?.message}</p>}
       </FieldSet>
       <button className='text-md text-white p-1 cursor-pointer rounded-lg bg-purple-500 m-auto'>
         Login
