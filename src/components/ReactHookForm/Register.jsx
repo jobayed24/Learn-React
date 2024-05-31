aa84179dcf040f39a25e57bc3ec408f3a3fb8324
jobayed24 <jkhouse117@gmail.com>
2024-05-31 20:45:12 +0600

    register: error validation and error message

--- src/components/ReactHookForm/Register.jsx
+++ src/components/ReactHookForm/Register.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function Register() {
-    const {register,handleSubmit}=useForm()
+    const {register,handleSubmit,formState: {errors}}=useForm()
     function handleOnSubmit(formData){
        console.log(formData)
     }
@@ -10,11 +10,31 @@ export default function Register() {
     <>
     <h1>Register Hook</h1>
      <form onSubmit={handleSubmit(handleOnSubmit)}>
+     <label>Name</label>
+     <input
+       type='name'
+       className='border-blue-500 border-2 rounded-md'
+       {...register('name',{
+        required: 
+        'value is required',
+        maxLength:
+        {
+            value: '5',
+            message: 'max Length 5'
+        },
+         minLength:
+        {
+            value: '2',
+            message: 'min length 2'
+        }
+        })}
+      />
+      <p>{errors?.name?.message}</p>
+     <label>Age</label>
      <input
        type='number'
-       value='4'
        className='border-blue-500 border-2 rounded-md'
-       {...register('date',{value: '43',valueAsNumber: true})}
+       {...register('age',{value: '27',valueAsNumber: true,deps:['name']})}
       />
       <input type='submit' />
      </form>
