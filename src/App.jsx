5ad2d5455a66409ec2579a8d3adbe30bc27ccd27
jobayed24 <jkhouse117@gmail.com>
2024-06-19 16:41:29 +0600

    pure components: componest should be pure 1.minds it's own business 2.same input sam output

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 
-function App() {
- 
+function MangosTree(){
 
+  return(
+   <>
+     <Mango mango={1} />
+    <Mango mango={2} />
+    <Mango mango={3} />
+   </>
+
+  )
+}
+
+function Mango(props){
+let mango=props.mango;
+  return(
+    <p>
+      mango serial is: {mango}
+    </p>
+  )
+}
+
+function App() {
+
+  
   return (
     <>
-      <h1>Hello, from React Js.</h1>
+    <MangosTree />
     </>
   )
 }
 
 export default App
+
